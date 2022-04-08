a62d57aafba6b7049eb237aba14c8777e19c28cf
salem z <salem.zerezghi2@gmail.com>
2022-04-07 22:10:58 -0400

    initial commit

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  fight();
+
+  var playerName = window.prompt("What's your robot's name?");
